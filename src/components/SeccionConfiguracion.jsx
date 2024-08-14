ec70149d528bead95293f45e82b872fde3e77790
b4ngi <ignaciobenetucci@gmail.com>
2024-08-14 11:47:08 -0300

    conexion con el backend para obtener datos de un compromiso de pago

--- src/components/SeccionConfiguracion.jsx
+++ src/components/SeccionConfiguracion.jsx
@@ -4,38 +4,11 @@ import React, { useState } from 'react';
 // Cuando se guarde un valor diferente hay que actualizar en la base de datos
 
 const SeccionConfiguracion = ({ texto, valorInicial }) => {
-  const [value, setValue] = useState(valorInicial);
-  const [isEditing, setIsEditing] = useState(false);
-
-  const handleToggle = () => {
-    setIsEditing(!isEditing);
-  };
-
-  const handleChange = (event) => {
-    setValue(event.target.value);
-  };
-
-  const handleSave = () => {
-    setIsEditing(false);
-  };
 
   return (
     <div className="item">
       <label>{texto}</label>
-      {isEditing ? (
-        <input
-          type="text"
-          value={value}
-          onChange={handleChange}
-          onBlur={handleSave}
-          autoFocus
-        />
-      ) : (
-        <span className="default-value">{value}</span>
-      )}
-      <button onClick={handleToggle}>
-        {isEditing ? 'Guardar' : 'Modificar'}
-      </button>
+      <label>{valorInicial}</label>
     </div>
   );
 };
--- src/pages/coordinador/Configuracion.jsx
+++ src/pages/coordinador/Configuracion.jsx
@@ -1,9 +1,33 @@
+import { useState, useEffect } from 'react';
 import SeccionConfiguracion from "../../components/SeccionConfiguracion";
 import Sidebar from "./SidebarCoordinador";
 import ImportarUsuarios from "../importaciones/ImportarUsuarios";
 import './Configuracion.css'
 
 function Configuracion() {
+
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('http://127.0.0.1:8000/api/v1/estudiantes/compromisoActual/');
+        if (!response.ok) {
+          throw new Error('Error en la solicitud');
+        }
+        const result = await response.json();
+        setData(result);
+      } catch (error) {
+        setError(error.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
 
     // A la SeccionConfiguracion se le tiene que pasar el valorInicial que es el valor que vamos a leer desde el servidor segun corresponda
     return (
@@ -12,13 +36,20 @@ function Configuracion() {
       
       <div className="contentConfig">
         <div className="containerConfig">
-          <SeccionConfiguracion texto="Valor de matricula" valorInicial={25000}/>
-          <SeccionConfiguracion texto="Valor de cuota completa" valorInicial={20000}/>
-          <SeccionConfiguracion texto="Valor de cuota reducida" valorInicial={15000}/>
-          <SeccionConfiguracion texto="Valor de primer mora" valorInicial={1000}/>
-          <SeccionConfiguracion texto="Valor de segunda mora" valorInicial={3000}/>
-          <ImportarUsuarios></ImportarUsuarios>
+          <h2>Compromiso de pago Actual</h2>
+          <br />
+          <SeccionConfiguracion texto="Valor de matricula" valorInicial={data.map((item) => (<li key={item.id}>{item.importe_matricula}</li>))}/>
+          <SeccionConfiguracion texto="Valor de cuota completa" valorInicial={data.map((item) => (<li key={item.id}>{item.importe_completo}</li>))}/>
+          <SeccionConfiguracion texto="Valor de cuota reducida" valorInicial={data.map((item) => (<li key={item.id}>{item.importe_reducido}</li>))}/>
+          <SeccionConfiguracion texto="Valor de primer mora completa" valorInicial={data.map((item) => (<li key={item.id}>{item.importe_pri_venc_comp}</li>))}/>
+          <SeccionConfiguracion texto="Valor de segunda mora completa" valorInicial={data.map((item) => (<li key={item.id}>{item.importe_seg_venc_comp}</li>))}/>
+          <SeccionConfiguracion texto="Valor de primer mora reducida" valorInicial={data.map((item) => (<li key={item.id}>{item.importe_pri_venc_red}</li>))}/>
+          <SeccionConfiguracion texto="Valor de segunda mora reducida" valorInicial={data.map((item) => (<li key={item.id}>{item.importe_seg_venc_red}</li>))}/>
+          <button>Cargar nuevo compromiso de pago</button>
+          <br />
+          <button>Ver historial de compromisos de pago</button>
         </div>
+        
       </div>
       </div>
     )

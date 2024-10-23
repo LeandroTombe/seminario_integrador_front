768b905f9f89f54857a00f93277cf28d8e4119fd
b4ngi <ignaciobenetucci@gmail.com>
2024-10-23 13:59:07 -0300

    algunas dependencias

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    './src/**/*.{js,jsx,ts,tsx}', // Archivos donde usas clases de Tailwind
+    './public/index.html',
+  ],
   theme: {
     extend: {},
   },
